7de735ac624cfbe0c82513a0cd639b889ba9df2c
yaserrifai <yaserwrifai@gmail.com>
2022-06-29 00:39:27 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+fetch ("https://www.breakingbadapi.com/api/characters/").then(
+  res=>{
+    res.json().then(
+      data=>{console.log(data);
+
+ if(data.length>0){var temp="";
+
+ //start for loop
+
+ data.forEach((u)=>{
+  temp +="<tr>";
+  temp +="<td>"+u.char_id+"</td>";
+  temp +="<td>"+u.name+"</td>";
+  temp +="<td>"+u.birthday+"</td>";
+ })
+//close for  loop
+
+document.getElementById("tdata").innerHTML=temp;
+
+}
+
+
+      }
+    )
+    }
+   
+    )
+    
+    $(document).ready(function(){
+      $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#tdata tr").filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+      });
+    });
